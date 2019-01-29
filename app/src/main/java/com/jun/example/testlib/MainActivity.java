1b7dc4dc936bf8af56f2dca6e976f31f3299ae47
15940363542@163.com <15940363542@163.com>
2019-01-29 13:38:11 +0800

    3

--- app/src/main/java/com/jun/example/testlib/MainActivity.java
+++ app/src/main/java/com/jun/example/testlib/MainActivity.java
@@ -2,6 +2,9 @@ package com.jun.example.testlib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.jun.example.mytestlib.TestUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("测试库", new TestUtils().getString() + "");
     }
 }
